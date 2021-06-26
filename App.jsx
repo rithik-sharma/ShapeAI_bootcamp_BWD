98b6a1e248da8f1c691442a7cff7c27363669354
rarerithik <78314622+rarerithik@users.noreply.github.com>
2021-06-26 17:14:57 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Footer from "./Footer";
+import Header from "./Header";
+import Info from "./Info";
+function app() {
+  return (
+    <div>
+      <Header />
+      <Footer />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+      <Info />
+    </div>
+  );
+}
+
+export default app;
